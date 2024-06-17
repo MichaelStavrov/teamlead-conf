2d4d2144acf4cb82d01a49f9827d3d767c9bbb08
m.stavrov <m.stavrov@vk.team>
2024-06-17 16:22:57 +0300

    v1.0.1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,9 @@
 import RootRoutes from 'routes/RootRoutes';
+import packageJSON from '../../../package.json';
 
 const App = () => {
+  console.log(`v${packageJSON.version}`);
+
   return <RootRoutes />;
 };
 
